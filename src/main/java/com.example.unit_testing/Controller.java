a879e22eeb098d208c61447908e69b7fe1638905
Vidyashree14 <39667604+Vidyashree14@users.noreply.github.com>
2018-06-03 09:41:50 +0400

    Create Controller.java

---
+++ src/main/java/com.example.unit_testing/Controller.java
@@ -0,0 +1,18 @@
+/**
+  * Created by Vidyashree on 05/31/2018.
+  */
+ 
+package com.example.unit_testing;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Controller {
+		
+		public String helloWorld() {
+			return "Hello World";
+		}
+		
+}
