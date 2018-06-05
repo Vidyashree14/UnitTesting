3b2867b766a671050997e0a6737cc366850aadd2
Vidyashree14 <39667604+Vidyashree14@users.noreply.github.com>
2018-06-05 12:40:47 +0400

    Create UserRepository.java

---
+++ src/main/java/com.example.unit_testing/UserRepository.java
@@ -0,0 +1,19 @@
+
+/**
+  * Created by Vidyashree on 05/31/2018.
+  */
+ 
+package com.example.unit_testing;
+
+import java.util.List;
+
+import org.apache.tomcat.jni.User;
+
+public class UserRepository {
+
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
